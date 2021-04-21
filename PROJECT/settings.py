7a221a911c8c9e6a144365db6a5b9b24787af230
FENR1 USER <fenr1user@biadsconsulting.org>
2021-04-21 22:56:49 +0600

    django authentication production setup

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -120,7 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR/ 'collectstatic'
+STATICFILES_DIRS = [
+    BASE_DIR/ 'static'
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- PROJECT/urls.py
+++ PROJECT/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app_auth.urls'))
 ]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
