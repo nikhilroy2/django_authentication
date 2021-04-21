771ed92e50d8b305593c00779b0bbe2bf5d53dda
FENR1 USER <fenr1user@biadsconsulting.org>
2021-04-21 23:13:16 +0600

    production

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6=z+w&c7dpne2b3nqx568b=c-_q%!z18$w!nhz=x6!f@i1^d0z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
