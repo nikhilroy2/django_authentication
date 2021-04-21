77821db9d39f5c224008449636fdbb0ca5a18af4
FENR1 USER <fenr1user@biadsconsulting.org>
2021-04-21 23:02:59 +0600

    all files

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/ 'collectstatic'
+STATIC_ROOT = BASE_DIR / 'collectstatic_files'
 STATICFILES_DIRS = [
-    BASE_DIR/ 'static'
+    BASE_DIR / "static"
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
