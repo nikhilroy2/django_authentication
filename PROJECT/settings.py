8897afe2d77f092d9c5ffd7a45e4bd76e28789a1
FENR1 USER <fenr1user@biadsconsulting.org>
2021-04-21 23:20:34 +0600

    static

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/'collectstatic_files'
+STATIC_ROOT = BASE_DIR / 'collectstatic_files'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
