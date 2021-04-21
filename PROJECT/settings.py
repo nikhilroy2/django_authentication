c965e4fd228f7d740d85509f7b63cf2c29385679
FENR1 USER <fenr1user@biadsconsulting.org>
2021-04-21 23:08:18 +0600

    static

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR/'collectstatic_files'
 STATICFILES_DIRS = [
-    BASE_DIR/"static"
+    BASE_DIR/"static","static"
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
